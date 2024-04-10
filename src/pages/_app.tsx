4abdc9a05efd36915dfb4b614324a8301015ea65
nikollllllas <nikollasohta2014@gmail.com>
2024-04-10 11:24:52 -0300

    feat: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 import { globalStyles } from '../styles/global'
+import igniteLogo from '../assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
@@ -11,9 +14,16 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image
+          src={igniteLogo}
+          alt=''
+        />
+      </Header>
+
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
 
