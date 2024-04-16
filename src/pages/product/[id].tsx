b016ac0260df9ca1a3cbf9d43107a48054a40c87
nikollllllas <nikollasohta2014@gmail.com>
2024-04-16 09:13:10 -0300

    feat: fallback w/ SSG and links prefetch

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -20,6 +20,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <h1>Carregando...</h1>
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -45,12 +51,8 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [
-      {
-        params: { id: 'prod_PuBRVnSG8BoVl1' }
-      }
-    ],
-    fallback: false
+    paths: [{ params: { id: 'prod_PuBRVnSG8BoVl1' }     }],
+    fallback: true
   }
 }
 
